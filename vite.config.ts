39cc5f270ab552e2966a5ddea9ab9d0828e36a97
Keita Ito <eguchi@eguchi.cc>
2024-08-28 00:29:10 +0900

    chore: ビルド構成の修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,11 @@
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import preserveDirectives from "rollup-plugin-preserve-directives";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: "use-client-banner",
-      generateBundle(_options, bundle) {
-        for (const key in bundle) {
-          const chunk = bundle[key];
-          if (chunk.type === "chunk") {
-            chunk.code = `"use client";\n${chunk.code}`;
-          }
-        }
-      },
-    },
-  ],
+  plugins: [react(), preserveDirectives()],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
@@ -27,7 +15,6 @@ export default defineConfig({
     rollupOptions: {
       output: {
         preserveModules: true,
-        preserveModulesRoot: "src",
         entryFileNames({ name }) {
           return `${name}.js`;
         },
